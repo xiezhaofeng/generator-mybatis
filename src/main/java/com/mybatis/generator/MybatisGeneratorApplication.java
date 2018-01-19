b8bba6a56d2a921c4b0cc842db422a65111f24cd
谢照锋 <xiezf@xunxintech.com>
2018-01-19 10:41:05 +0800

    init generator

---
+++ src/main/java/com/mybatis/generator/MybatisGeneratorApplication.java
@@ -0,0 +1,30 @@
+package com.mybatis.generator;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MybatisGeneratorApplication {
+
+
+	public static void main(String[] args) throws Exception {
+
+		String path = Thread.currentThread().getContextClassLoader().getResource("generatorConfig.xml").getPath();
+
+		File configFile = new File(path);
+
+		System.out.println(path);
+		List<String> warnings = new ArrayList<String>();
+		boolean overwrite = true;
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(configFile);
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+		myBatisGenerator.generate(null);
+	}
+}
